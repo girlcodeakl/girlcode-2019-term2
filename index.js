ea13ad3bd044566b4b815a56acc8763a5d0ce9bf
amri-888 <51732027+amri-888@users.noreply.github.com>
2019-06-27 20:03:51 +1200

    Author display

--- index.js
+++ index.js
@@ -22,8 +22,11 @@ app.get('/posts', sendPostsList);
 
 //let a client POST something new
 function saveNewPost(request, response) {
-  console.log(request.body.message); //write it on the command prompt so we can see
+  console.log(request.body.message); 
+  console.log(request.body.author);
   let post= {};
+  post.author=request.body.author;//write it on the command prompt so we can see
+  
 post.message = request.body.message;
 posts.push(post); //save it in our list
   response.send("thanks for your message. Press back to add another");
